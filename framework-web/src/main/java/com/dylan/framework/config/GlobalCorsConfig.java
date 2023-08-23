acb4b406fbac16333775ed1e8318457b5359d170
Dylan <1171357812@qq.com>
2023-08-24 00:32:52 +0800

    feat: 允许所有域的请求

--- framework-web/src/main/java/com/dylan/framework/config/GlobalCorsConfig.java
+++ framework-web/src/main/java/com/dylan/framework/config/GlobalCorsConfig.java
@@ -21,14 +21,15 @@ public class GlobalCorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 //添加映射路径
                 registry.addMapping("/**")
-                        //放行哪些原始域
-                        .allowedOriginPatterns("*://localhost:*", "*://logicer.top:*")
-                        //是否发送Cookie信息
-                        .allowCredentials(true)
+                        .allowedOrigins("*")
                         //放行哪些原始域(请求方式)
-                        .allowedMethods("GET","POST", "PUT", "DELETE")
+                        .allowedMethods("GET","POST", "PUT", "DELETE","OPTIONS")
                         //放行哪些原始域(头部信息)
                         .allowedHeaders("*");
+                        //是否发送Cookie信息
+                        //.allowCredentials(true)
+                        //放行哪些原始域
+                        //.allowedOriginPatterns("*://localhost:*", "*://logicer.top:*", "*logicer.top*")
                         //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
                         //.exposedHeaders("Header1", "Header2");
             }
