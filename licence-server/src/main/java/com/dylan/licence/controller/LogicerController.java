8d808d7727b5946e9fe3926be47ab56b3611871b
duanqilun <duanqilun@mail.jiehun.com>
2023-08-29 10:00:54 +0800

    FEAT: HomeDataLoader

--- licence-server/src/main/java/com/dylan/licence/controller/LogicerController.java
+++ licence-server/src/main/java/com/dylan/licence/controller/LogicerController.java
@@ -2,6 +2,8 @@ package com.dylan.licence.controller;
 
 import com.dylan.framework.model.result.DataResult;
 import com.dylan.framework.model.result.HttpResult;
+import com.dylan.licence.config.HomeDataLoader;
+import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -9,9 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @Classname LogicerController
@@ -22,17 +29,8 @@ import java.util.Map;
 @RequestMapping("logicer")
 public class LogicerController {
 
-    @Autowired
-    private Environment environment;
-
-    @Value("${logicer.home.data.title: 学海无涯}")
-    private String homeDataTitle;
-
-    @Value("#{'${logicer.home.data.favors: Java,React,Harmony,Esp32,TCP,Debian CentOS Ubuntu}'.split(',')}")
-    private List<String> favors;
-
-    @Value("#{${logicer.home.data.proverbs: {first:'枕月绾袖临风 扣舷独饮千钟', second:'不见韶华白首 浮生一梦从容'}}}")
-    private Map<String, String> proverbs;
+    @Resource
+    private HomeDataLoader homeDataLoader;
 
     /**
      * 分页获取用户组
@@ -41,9 +39,15 @@ public class LogicerController {
     @GetMapping("get-home-data")
     public HttpResult getPagedGroup() {
         Map<String, Object> homeDataMap = new HashMap<>();
-        homeDataMap.put("title", homeDataTitle);
-        homeDataMap.put("favors", favors);
-        homeDataMap.put("proverbs", proverbs);
+        String defaultTitle = "LOGICER";
+        List<String> defaultFavors = Arrays.asList("P1","P2","P3","P4","P5","P6");
+        Map<String,String> defaultProverbs = Stream.of(new String[][] {
+                { "first", "天" },
+                { "second", "地" },
+        }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
+        homeDataMap.put("title", Objects.isNull(homeDataLoader.getTitle()) ? defaultTitle : homeDataLoader.getTitle());
+        homeDataMap.put("favors", Objects.isNull(homeDataLoader.getFavors()) ? defaultFavors : homeDataLoader.getFavors());
+        homeDataMap.put("proverbs", Objects.isNull(homeDataLoader.getProverbs()) ? defaultProverbs : homeDataLoader.getProverbs());
         return DataResult.success().data(homeDataMap).build();
     }
 
