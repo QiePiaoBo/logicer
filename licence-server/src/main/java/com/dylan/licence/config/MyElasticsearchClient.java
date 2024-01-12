47bb0a500f1a432fcb3b243b1cb09e62177e814e
Dylan <1171357812@qq.com>
2024-01-13 01:53:09 +0800

    feat: licence集成Elasticsearch es版本号为7.17.3

---
+++ licence-server/src/main/java/com/dylan/licence/config/MyElasticsearchClient.java
@@ -0,0 +1,42 @@
+package com.dylan.licence.config;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyElasticsearchClient {
+
+    @Value("${spring.elasticsearch.username}")
+    private String userName;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+
+    @Bean
+    public RestHighLevelClient esRestClient() {
+        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(userName, password));
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost("172.26.0.150", 9200, "http"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+    }
+
+}
